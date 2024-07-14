87ae87d7df5219c7cba448ecb656b8c73fafb420
Affaan <141487534+melonpan1007@users.noreply.github.com>
2024-07-14 19:26:13 +0530

    Add files via upload

---
+++ Tetris/Tetris/BlockQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock(),
+        };
+
+        private readonly Random random = new Random();
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = GetRandomBlock();
+        }
+
+        private Block GetRandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdateNextBlock()
+        {
+            Block currentBlock = NextBlock;
+            do
+            {
+                NextBlock = GetRandomBlock();
+            } while (Block.Id == NextBlock.Id);  // Ensure no consecutive duplicates
+
+            return currentBlock;
+        }
+    }
+}
